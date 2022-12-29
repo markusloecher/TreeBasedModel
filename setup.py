8a94f9b308d7a9b006cd6ecaec40d86d997a5cb3
Philipp <philipp.heitmann94@gmail.com>
2022-12-29 08:25:09 +0100

    Create script to run performance comparison experiment

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='SelectiveHierarchicalShrinkage',
       version="0.1",
-      description="Accompanining package for master thesis",
+      description="Accompaning package for master thesis",
       packages=find_packages(),
       install_requires=requirements,
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/HS_performance_rf-run', 'scripts/HS_performance_HSnodewise'],
+      scripts=['scripts/Run_PredPerf_experiment', 'scripts/HS_performance_rf-run', 'scripts/HS_performance_HSnodewise'],
       zip_safe=False)

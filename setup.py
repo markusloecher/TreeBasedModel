25577607850519d9c873e6ed7812d8444a8cd3d0
Philipp Heitmann <philipp.heitmann94@gmail.com>
2023-01-17 16:39:19 +0100

    Change setup name

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='SelectiveHierarchicalShrinkage',
-      version="0.1",
-      description="Accompaning package for master thesis",
+setup(name='AugmentedHierarchicalShrinkage',
+      version="1",
+      description="Python package for master thesis",
       packages=find_packages(),
       install_requires=requirements,
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/Run_PredPerf_experiment', 'scripts/HS_performance_rf-run', 'scripts/HS_performance_HSnodewise'],
+      scripts=['scripts/Run_PredPerf_experiment'],
       zip_safe=False)
